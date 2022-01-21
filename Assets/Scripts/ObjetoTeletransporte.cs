87c78aa22604d3709bc8d9ce7e8ae06e01845067
alu0100888157 <alu0100888157@ull.edu.es>
2022-01-21 09:09:13 +0000

    commit
    COMMIT

--- Assets/Scripts/ObjetoTeletransporte.cs
+++ Assets/Scripts/ObjetoTeletransporte.cs
@@ -6,8 +6,8 @@ public class ObjetoTeletransporte : MonoBehaviour
 {
     public GameObject jugador;
     public GameObject jugador_posicion;
-    public GameObject camara;
     public Vector3 posicion;
+    public GameObject arbol;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +21,12 @@ public class ObjetoTeletransporte : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Entra en función");
-        if (collision.collider == jugador.gameObject.GetComponentsInChildren<BoxCollider>()[0])
+        Debug.Log("Entra en función  " + posicion.x.ToString() + "   " + posicion.y.ToString() + "  " + posicion.z.ToString() + "");
+        if (collision.collider == jugador.gameObject.GetComponent<BoxCollider>())
         {
-            Debug.Log("Entra en if");
-            jugador_posicion.transform.position = posicion;
+            Debug.Log("Entra en if dentro del colisión");
+            jugador_posicion.transform.position = arbol.transform.position;
+            Debug.Log("sale en if dentro del colisión");
             //jugador.transform.position = new Vector3(0f, 1.04f,0);
             //Vector3 posicion_camara = new Vector3(0.1999969f, 1.9f, -4.096f);
             //camara.transform.position = posicion;//+ posicion_camara;
